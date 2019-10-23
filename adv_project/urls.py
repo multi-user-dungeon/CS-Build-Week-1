7d3847f615c615e578918418d8cc703779b39c34
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2019-10-23 13:15:19 -0400

    created login endpoint

--- adv_project/urls.py
+++ adv_project/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import include
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api/adv/', include('adventure.urls')),
+    path('login', obtain_auth_token, name='api_token_auth'),
 ]
